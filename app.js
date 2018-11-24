3b7365b47a5441493c8ca131ac537cc24137480a
Alex Cho <alexsjcho@gmail.com>
2018-11-24 13:06:06 -0800

    2nd commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let companyName = "Covalence Bootcamp";
+let jobTitle = "Software Developer";
+let description = "Build using full-stack Javascript technologies";
+let skills = ["Mandarin", "Javascript","Sales", "Ruby-On-Rails", "Python", "Singing", "Guitar", "Breakdance"];
+
+function displayPosition(companyName, jobTitle, description) {
+    console.log(displayPosition);
+}
+
+function displaySkills(skills) {
+if (skills == true)
+    return "BAM:";
+} //else {
+//     return "That\s cool too";
+// }
