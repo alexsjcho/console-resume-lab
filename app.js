fbb7cba805792fdba7c66440a57be8d1e47fbeaf
Alex Cho <alexsjcho@gmail.com>
2018-11-25 10:14:06 -0800

    still working on it

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-let companyName = "Covalence Bootcamp";
-let jobTitle = "Software Developer";
-let description = "Build using full-stack Javascript technologies";
-let skills = ["Mandarin", "Javascript","Sales", "Ruby-On-Rails", "Python", "Singing", "Guitar", "Breakdance"];
+//Function Display Position
+const positionTitle = "Me and My Role:";
+    console.log(positionTitle);
+function displayPosition(name, jobTitle, description) {
+    for (i = 0; i < displayPosition.length; i++){
+        const name = "Name:"+"Alex Cho";
+        const jobTitle = "Career: Software Developer";
+        const description = "Description: Build using full-stack Javascript technologies";
+        return name + jobTitle + description;
+    }
+    console.log("Name:"+"Alex Cho".toUpperCase());
+}
+console.log(displayPosition());
 
-function displayPosition(companyName, jobTitle, description) {
-    console.log(displayPosition);
+//List of Interests
+const interestsTitle = "My Interests:";
+    console.log(interestsTitle);
+const interests = ["*Movies", "*Travels","*Creating Awkward Situations","*Playing Video Games"];
+for (i = 0; i < interests.length; i++){
+        console.log(interests[i]);
 }
 
-function displaySkills(skills) {
-if (skills == true)
-    return "BAM:";
-} //else {
-//     return "That\s cool too";
-// }
+//Previous Experience
+const experiencesTitle = "My Previous Experiences:";
+    console.log(experiencesTitle);
+const experiences = ["*Agapeland - Music Teacher - Taught elementary to high school students pragmatic music theory and singing", 
+
+"*15Five, IFTTT - Salesperson - worked at startups in different sales capacity roles i.e. lead generation, revenue generator, post-sales",
+
+"*Galileo High School - Breakdance Teacher - Taught students fundamentals of breakdancing, exposed them to hip-hop scene in San Francisco Bay Area"];
+
+for (i = 0; i < experiences.length; i++){
+    console.log(experiences[i]);
+}
+
+//Display Skills
+const skillsTitle = "My Skills:";
+    console.log(skillsTitle);
+const skills = ["*Mandarin", "*Javascript","*Sales", "*Ruby-On-Rails", "*Python", "*Singing", "*Guitar", "*Breakdance"];
+for (i = 0; i < skills.length; i++){
+    console.log(skills[i]);
+}
+   
+//Function Display BAM Skills
+function displaySkills(coolSkill) {
+    for (let c = 0; c <coolSkill.length; i++ ){
+        if (coolSkill) {
+            ku(coolSkill(-1));
+            console.log("BAM: %s", skills);
+        } 
+    }
+} 
+
+
+
